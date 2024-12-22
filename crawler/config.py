86098b60be51fda9347b1ec19924af60f2b81cb9
T-WK <ktwqwe12@daum.net>
2024-12-22 16:25:20 +0900

    게시판 데이터 크롤링을 위한 설정 상수 추가

--- crawler/config.py
+++ crawler/config.py
@@ -2,3 +2,12 @@ class Config:
     ROOT_RUL = "https://soco.seoul.go.kr/youth/bbs/BMSR00015/"
     ANNOUNCEMENT_LINK = "https://soco.seoul.go.kr/youth/bbs/BMSR00015/list.do?menuNo=400008"
     
+    #csv 파일명
+    CSV_FILENAME = "./crawler/board_data.csv"
+
+    # dict key
+    POST_NUMBER_KEY = "글번호"
+    POST_TITLE_KEY = "제목"
+    POST_URL_KEY = "글링크"
+    POST_DATE_KEY = "게시일"
+    APPLICATION_DATE_KEY = "신청일"
