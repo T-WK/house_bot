06f7857f812b8e3379021ba346278c32c876fa67
T-WK <ktwqwe12@daum.net>
2024-12-25 22:58:27 +0900

    logo 생성 로직 구현
    - 로고에 타임 찍히도록 구현함

--- crawler/logo_handler.py
+++ crawler/logo_handler.py
@@ -1,2 +1,21 @@
+from config import Config
+import time
+
+
+
 class LogoHandler:
-    pass
+    def write_logo(is_timespan_logo, text):
+        
+        logo_text = ""
+        
+        if (is_timespan_logo):
+            str_timespan = time.strftime('[%Y-%m-%d %H:%M:%S]')
+            logo_text = str_timespan + ' - ' + text
+
+        else:
+            logo_text = text
+
+
+        with open(Config.LOGO_FILE_PATH, mode='a', encoding='utf-8') as file:
+            file.write(logo_text)
+
