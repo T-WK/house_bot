d12b8e4d643a69641d21102f567a18545d77123a
whan0767 <whan0767@naver.com>
2024-12-30 18:28:53 +0900

    selenium still not working

--- crawler/config.py
+++ crawler/config.py
@@ -3,7 +3,7 @@ class Config:
     ANNOUNCEMENT_LINK = "https://soco.seoul.go.kr/youth/bbs/BMSR00015/list.do?menuNo=400008"
     
     #csv 파일명
-    CSV_FILENAME = "./crawler/board_data.csv"
+    CSV_FILENAME = "./board_data.csv"
 
     # dict key
     POST_NUMBER_KEY = "post_number"
